36be273265d08ea36f9c4b590ffa007b4e4a1dec
Kartik <kartik.bisht@dianapps.com>
2025-09-19 17:00:03 +0530

    updated

--- src/Controller/managementController.js
+++ src/Controller/managementController.js
@@ -40,19 +40,4 @@ const currentLeaveRequests = async (req, res) => {
     }
 }
 
-const getAllEmployeesDetails = async (req, res) => {
-
-    try {
-        let docs = await Employee.find({ $or: [{ role: "HR" }, { role: "employee" }] }, { name: 1, role: 1 }).sort({ role: 1 })
-
-        res.status(200);
-        return res.json({ data: docs, message: "All Employees data Fetched successfully..." })
-    }
-    catch (error) {
-        console.log(error.message);
-        res.status(500);
-        return res.json({ message: "Error occurred while fetching all details of employees..." })
-    }
-}
-
 module.exports = { pastLeave, currentLeaveRequests, getAllEmployeesDetails }
